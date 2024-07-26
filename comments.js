538bba32a3b8bbc599e5cf31a2915c9103fdafbc
MashiMaro72 <161359007+MashiMaro72@users.noreply.github.com>
2024-07-26 12:31:46 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,8 +1,51 @@
 // create web server
-// create a route for comments
-// create a route for comments/:id
-// create a route for comments/:id/edit
-// create a route for comments/:id/delete
-// create a route for comments/new
-// create a route for comments/create
-// create a route for comments/:id/update
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var comments = [];
+
+var server = http.createServer(function(request, response) {
+    var parseObj = url.parse(request.url, true);
+    var pathName = parseObj.pathname;
+    if (pathName === '/') {
+        fs.readFile('./views/index.html', function(err, data) {
+            if (err) {
+                return response.end('404 Not Found.');
+            }
+            response.end(data);
+        });
+    } else if (pathName === '/post') {
+        fs.readFile('./views/post.html', function(err, data) {
+            if (err) {
+                return response.end('404 Not Found.');
+            }
+            response.end(data);
+        });
+    } else if (pathName.indexOf('/public/') === 0) {
+        fs.readFile('.' + pathName, function(err, data) {
+            if (err) {
+                return response.end('404 Not Found.');
+            }
+            response.end(data);
+        });
+    } else if (pathName === '/comment') {
+        var comment = parseObj.query;
+        comment.dateTime = '2018-12-20 15:24:00';
+        comments.push(comment);
+        response.statusCode = 302;
+        response.setHeader('Location', '/');
+        response.end();
+    } else {
+        fs.readFile('./views/404.html', function(err, data) {
+            if (err) {
+                return response.end('404 Not Found.');
+            }
+            response.end(data);
+        });
+    }
+});
+
+server.listen(3000, function() {
+    console.log('Server is running...');
+});
